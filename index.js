c16189d07ac9deebaea85b2dec48a2d52cbd10fd
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-24 17:00:19 -0700

    index.js
    Updated file, finished part 1 of proyect

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ const express = require("express");
 const app = express();
 
 const url = "https://www.theguardian.com/uk";
-axios(url).then((response) => {
-  const html = response.data;
-  console.log(html);
-});
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+
+    $(".fc-item__title", html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+    });
+    console.log(articles);
+  })
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
