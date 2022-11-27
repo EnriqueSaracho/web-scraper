e642ad8b7ea7797776e427cbc898d7d731694b96
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-26 20:09:46 -0700

    src/app.js
    Updated file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 const feedDisplay = document.querySelector("#feed");
 
 fetch("http://localhost:8000/results")
-  .then((response) => response.json)
-  .then((data) => console.log(data));
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((article) => {
+      const articleItem =
+        "<div><h3>" +
+        article.title +
+        "</h3><br><h4>" +
+        article.url +
+        "</h4></div>";
+      feedDisplay.insertAdjacentHTML("beforeend", articleItem);
+    });
+  })
+  .catch((err) => console.log(err));
